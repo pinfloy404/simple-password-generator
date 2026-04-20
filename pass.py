09bd9c5ec35e16ea5b20f4ed4e2dcb70745f18f3
pinfloy <277553290+pinfloy404@users.noreply.github.com>
2026-04-20 17:38:36 +0200

    length argument and program restructuring

--- pass.py
+++ pass.py
@@ -1,15 +1,34 @@
+import argparse
 import string
 import secrets
 
-#   Variables
-password = ""
+#   Argument parser function
+def get_args():
+    #   Create parser object
+    parser = argparse.ArgumentParser(prog='Simple Password Generator', description='A simple password generator in Python.')
+    
+    #   Length argument
+    parser.add_argument('-l', '--length', type=int, default=16, help='Password length. By default it\'s 16 characters')
 
-#   List of characters
-characters_list = string.ascii_letters + string.digits + string.punctuation
+    #   Returns parsed arguments
+    return parser.parse_args()
 
-#   Generator loop
-for _ in range(16):
-    password += secrets.choice(characters_list)
+#   Main function
+def main():
+    #   Variables
+    args = get_args()
+    password = ""
 
-#   Show generated password
-print(password)
+    #   List of characters
+    characters_list = string.ascii_letters + string.digits + string.punctuation
+
+    #   Generator loop
+    for _ in range(args.length):
+        password += secrets.choice(characters_list)
+
+    #   Show generated password
+    print(password)
+
+#   Run main fuction
+if __name__ == "__main__":
+    main()
