d96593b14d445b2189f5efed6138ba1ee302e518
pinfloy <277553290+pinfloy404@users.noreply.github.com>
2026-04-21 22:14:24 +0200

    modularization

--- pass.py
+++ pass.py
@@ -3,6 +3,9 @@ import sys
 import string
 import secrets
 
+#   Character list length constant
+CH_LIST_LENGTH = 4
+
 #   Argument parser function
 def get_args():
     #   Create parser object
@@ -14,20 +17,70 @@ def get_args():
     #   Returns parsed arguments
     return parser.parse_args()
 
+#   Function to add the extra numbers of mod operation to the characters dict
+def extra_selection(args: argparse.Namespace, characters_dict: dict[str, int]):
+    #   Get the extra numbers of mod operation
+    extra = int(args.length % CH_LIST_LENGTH)
+
+    #   Dictionary to list
+    characters_list = list(characters_dict)
+
+    #   Random sum of extra numbers for character type selection
+    while extra > 0:
+        #   Create a number range inside extra variable range (1 -> extra)
+        extra_range = (secrets.randbelow(extra) + 1)
+
+        #   Get random index from dictionary
+        index = secrets.randbelow(len(characters_dict))
+
+        #   Get key
+        key = characters_list[index]
+
+        #   Change key's value by adding the number range
+        value = (characters_dict.get(key) + extra_range)
+
+        #   Adding key's modified value
+        characters_dict[key] = value
+
+        #   Removing the range used
+        extra -= extra_range
+
+#   Function to password generation
+def password_generator(args: argparse.Namespace, characters_dict: dict[str, int]) -> str:
+    #   Password string
+    password = ""
+
+    #   Dictionary to list
+    characters_list = list(characters_dict)
+
+    #   Generator loop
+    for _ in range(args.length):
+        #   Get random index from dictionary
+        index = secrets.randbelow(len(characters_dict))
+
+        #   Get key
+        key = characters_list[index]
+
+        #   Change key's value by adding the number range
+        value = (characters_dict.get(key) - 1)
+
+        #   Adding key's modified value
+        characters_dict[key] = value
+
+        #   Adding a character to password string
+        password += secrets.choice(characters_list[index])
+
+        #   If selection number is 0, the selected string isn't in use anymore
+        if value == 0:
+            characters_dict.pop(key)
+
+    #   Return password generated
+    return password
+
 #   Main function
 def main():
     #   Get arguments
     args = get_args()
-
-    #   Password string
-    password = ""
-
-    #   Character list length constant
-    CH_LIST_LENGTH = 4
-
-    #   Divide the password length into character list length and saves the extra from mod
-    selection = int(args.length / CH_LIST_LENGTH)
-    extra = int(args.length % CH_LIST_LENGTH)
 
     #   Minimum length check
     if args.length < 12:
@@ -36,49 +89,17 @@ def main():
         #   Script exit
         sys.exit(1)
 
+    #   Divide the password length into character list length and saves the extra from mod
+    selection = int(args.length / CH_LIST_LENGTH)
+
     #   Dictionary of characters
-    characters_list = {string.ascii_lowercase : selection, string.ascii_uppercase : selection, string.digits : selection, string.punctuation : selection}
+    characters_dict = {string.ascii_lowercase : selection, string.ascii_uppercase : selection, string.digits : selection, string.punctuation : selection}
 
-    #   Random sum of extra numbers for character type selection
-    while extra > 0:
-        #   Create a number range inside extra variable range (1 -> extra)
-        extra_range = (secrets.randbelow(extra) + 1)
+    #   Calls extra numbers function
+    extra_selection(args, characters_dict)
 
-        #   Get random index from dictionary
-        index = secrets.randbelow(len(characters_list))
-
-        #   Get key
-        key = list(characters_list)[index]
-
-        #   Change key's value by adding the number range
-        value = (characters_list.get(key) + extra_range)
-
-        #   Adding key's modified value
-        characters_list[key] = value
-
-        #   Removing the range used
-        extra -= extra_range
-
-    #   Generator loop
-    for _ in range(args.length):
-        #   Get random index from dictionary
-        index = secrets.randbelow(len(characters_list))
-
-        #   Get key
-        key = list(characters_list)[index]
-
-        #   Change key's value by adding the number range
-        value = (characters_list.get(key) - 1)
-
-        #   Adding key's modified value
-        characters_list[key] = value
-
-        #   Adding a character to password string
-        password += secrets.choice(list(characters_list)[index])
-
-        #   If selection number is 0, the selected string isn't in use anymore
-        if value == 0:
-            characters_list.pop(key)
+    #   Calls password generator
+    password = password_generator(args, characters_dict)
 
     #   Show generated password
     print(f"{password}")
