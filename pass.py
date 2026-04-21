59372641f11c34887088adf2589db94e884a217e
pinfloy <277553290+pinfloy404@users.noreply.github.com>
2026-04-21 18:08:48 +0200

    character selection limits I

--- pass.py
+++ pass.py
@@ -16,24 +16,59 @@ def get_args():
 
 #   Main function
 def main():
-    #   Variables
+    #   Get arguments
     args = get_args()
+
+    #   Password string
     password = ""
+
+    #   Character list length constant
+    CH_LIST_LENGTH = 4
+
+    #   Divide the password length into character list length and saves the extra from mod
+    selection = int(args.length / CH_LIST_LENGTH)
+    extra = int(args.length % CH_LIST_LENGTH)
 
     #   Minimum length check
     if args.length < 12:
         print(f'arg error: length -> {args.length} < 12')
+
+        #   Script exit
         sys.exit(1)
 
-    #   List of characters
-    characters_list = string.ascii_letters + string.digits + string.punctuation
+    #   Dictionary of characters
+    characters_list = {string.ascii_lowercase : selection, string.ascii_uppercase : selection, string.digits : selection, string.punctuation : selection}
+
+    #   Random sum of extra numbers for character type selection
+    while extra > 0:
+        #   Create a number range inside extra variable range (1 -> extra)
+        extra_range = (secrets.randbelow(extra) + 1)
+
+        #   Get random index from dictionary
+        index = secrets.randbelow(len(characters_list))
+
+        #   Get key
+        key = list(characters_list)[index]
+
+        #   Change key's value by adding the number range
+        value = (characters_list.get(key) + extra_range)
+
+        #   Adding key's modified value
+        characters_list[key] = value
+
+        #   Removing the range used
+        extra -= extra_range
 
     #   Generator loop
     for _ in range(args.length):
-        password += secrets.choice(characters_list)
+        #   Get random index from dictionary
+        index = secrets.randbelow(len(characters_list))
+
+        #   Adding a character to password string
+        password += secrets.choice(characters_list[index])
 
     #   Show generated password
-    print(password)
+    print(f"{password} / {len(password)} / {extra}")
 
 #   Run main fuction
 if __name__ == "__main__":
