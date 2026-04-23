036bb695cd4b180d417413debb52f306112b964a
pinfloy <277553290+pinfloy404@users.noreply.github.com>
2026-04-23 18:21:14 +0200

    reducing over-engineering

--- pass.py
+++ pass.py
@@ -10,12 +10,6 @@ RED = '\033[91m'
 YELLOW = '\033[93m'
 RESET = '\033[0m'
 
-#   Character list length constant
-CH_LIST_LENGTH = 4
-
-#   Character list total length
-CH_TOTAL_LENGTH = len(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
-
 #   Argument parser function
 def get_args():
     #   Create parser object
@@ -30,69 +24,18 @@ def get_args():
     #   Returns parsed arguments
     return parser.parse_args()
 
-#   Function to add the extra numbers of mod operation to the characters dict
-def extra_selection(args: argparse.Namespace, characters_dict: dict[str, int]):
-    #   Get the extra numbers of mod operation
-    extra = int(args.length % CH_LIST_LENGTH)
-
-    #   Dictionary to list
-    characters_list = list(characters_dict)
-
-    #   Random sum of extra numbers for character type selection
-    while extra > 0:
-        #   Create a number range inside extra variable range (1 -> extra)
-        extra_range = (secrets.randbelow(extra) + 1)
-
-        #   Get random index from dictionary
-        index = secrets.randbelow(len(characters_list))
-
-        #   Get key
-        key = characters_list[index]
-
-        #   Change key's value by adding the number range
-        value = (characters_dict.get(key) + extra_range)
-
-        #   Adding key's modified value
-        characters_dict[key] = value
-
-        #   Removing the range used
-        extra -= extra_range
-
 #   Function to password generation
-def password_generator(args: argparse.Namespace, characters_dict: dict[str, int]) -> str:
+def password_generator(args: argparse.Namespace, characters_list: list[str]) -> str:
     #   Password string
     password = ""
-
-    #   Dictionary to list
-    characters_list = list(characters_dict)
 
     #   Generator loop
     for _ in range(args.length):
         #   Get random index from dictionary
         index = secrets.randbelow(len(characters_list))
 
-        #   Get key
-        key = characters_list[index]
-
-        #   Change key's value by reducing the number range
-        value = (characters_dict.get(key) - 1)
-
-        #   Adding key's modified value
-        characters_dict[key] = value
-
-        #   Get random character
-        character = secrets.choice(characters_list[index])
-
-        #   Adding a character to password string
-        password += character
-
-        #   If password length is less than 94 (all ascii strings combined), then it can have unique letters
-        if args.length <= CH_TOTAL_LENGTH:
-            characters_list[index].replace(character, '')
-
-        #   If selection number is 0, the selected string isn't in use anymore
-        if value == 0:
-            characters_list.remove(key)
+        #   Adding a random character to password string
+        password += secrets.choice(characters_list[index])
 
     #   Returns password generated
     return password
@@ -101,7 +44,10 @@ def password_generator(args: argparse.Namespace, characters_dict: dict[str, int]
 def print_password(args: argparse.Namespace, password: str):
     #   If color argument is False, prints the password and exits correctly
     if args.color is False:
-        print(password) 
+        #   Shows uncolored  password
+        print(password)
+
+        #   Script exit correctly
         sys.exit(0)
 
     #   Colored password string
@@ -141,20 +87,14 @@ def main():
     if args.length < 12:
         print(f'arg error: length -> {args.length} < 12')
 
-        #   Script exit
+        #   Script exit with error
         sys.exit(1)
 
-    #   Divides the password length into character list length and saves the extra from mod
-    selection = int(args.length / CH_LIST_LENGTH)
-
-    #   Dictionary of characters
-    characters_dict = {string.ascii_lowercase : selection, string.ascii_uppercase : selection, string.digits : selection, string.punctuation : selection}
-
-    #   Calls extra numbers function
-    extra_selection(args, characters_dict)
+    #   List of characters
+    characters_list = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
 
     #   Calls password generator function
-    password = password_generator(args, characters_dict)
+    password = password_generator(args, characters_list)
 
     #   Calls password coloring function
     print_password(args, password)
